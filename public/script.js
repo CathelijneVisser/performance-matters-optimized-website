ced1df65ce007993792c3614a914dc6c1d34be73
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-04-24 12:18:59 +0200

    map werkt

--- public/script.js
+++ public/script.js
@@ -1,21 +1,34 @@
 // Huidige datum automatisch inladen
-const dateInput = document.getElementById('date-from');
-const dateOutput = document.getElementById("date-till");
+const dateInput = document.getElementById('date-from')
+const dateOutput = document.getElementById("date-till")
 
 // Automatisch einddatum meeveranderen
-dateInput.onchange = () => {
-    dateOutput.value = dateInput.value
+if(dateInput) {
+    dateInput.onchange = () => {
+        dateOutput.value = dateInput.value
+    }
 }
 
 // Invalid
-const inputs = document.querySelectorAll("input, select, textarea"); 
+const inputs = document.querySelectorAll("input, select, textarea")
 
 inputs.forEach(input => { 
     input.addEventListener( 
         "invalid", 
         event => { 
-            input.classList.add("invalid"); 
+            input.classList.add("invalid")
         }, 
         false 
     ); 
 });
+
+//map
+
+//map maken
+var map = L.map('map').setView([52.2129919, 5.2793703], 7)
+
+L.tileLayer('https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=6ws7wF90PofV2LnNfOXL', {
+    maxZoom: 19,
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+}).addTo(map);
+
